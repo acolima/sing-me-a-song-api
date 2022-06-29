57b8619e8976ac3d2bb67170501e9bc139593985
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-06-29 14:24:04 -0300

    Preparando revisão de código

--- tests/factories/recommendationFactory.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from '../../src/database.js'
-import { CreateRecommendationData } from '../../src/services/recommendationsService'
-
-export async function recommendationFactory(song: CreateRecommendationData) {
-	return await prisma.recommendation.create({ data: song })
-}
