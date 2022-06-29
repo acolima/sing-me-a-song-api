62e3083e67080bb98ede328cc22f18b88657a5c5
iagod <iago.tostes@cubos.io>
2022-06-29 10:27:18 -0300

    Preparando revisão de código

--- tests/factories/recommendationResponseFactory.ts
+++
@@ -1,8 +0,0 @@
-export function recommendationResponseFactory(score: number) {
-	return {
-		id: 5,
-		name: 'Lost Cause - Billie Eilish',
-		youtubeLink: 'https://www.youtube.com/watch?v=S2dRcipMCpw',
-		score,
-	}
-}
